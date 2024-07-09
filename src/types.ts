ef36e6ca4a43b96e8314562cb324924b596c25d9
Luxtar90 <luxtar9@gamil.com>
2024-07-09 12:23:25 -0500

    CORRECCION DEL EDIT

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
+// src/types.ts
 export interface Task {
   _id: string;
   title: string;
   description: string;
+  start?: string;  // Asegúrate de que estos campos estén presentes
+  end?: string;    // Asegúrate de que estos campos estén presentes
   completed: boolean;
-  dueDate?: Date;
 }
